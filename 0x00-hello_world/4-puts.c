551c54e1a0f98d3fd36d4407f21d8e4561e2fd53
arnoldmn <arnoldnzibuk@gmail.com>
2023-03-16 20:01:59 +0000

    Fix error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 /**
- * Description:  prints a basic C program using the puts function
+ * main - Entry point
  *
- * main - is where out program starts from
- *
- * Return: (0) means that we end our program stops there 
- *
+ * Return: Always (0) success
  */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 
 	return (0);
 }

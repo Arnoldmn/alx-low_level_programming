0f0150f1d18431e42a4b88faa67c85f1ce5545cc
arnoldmn <arnoldnzibuk@gmail.com>
2023-03-16 19:50:09 +0000

    Puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * Description:  prints a basic C program using the puts function
+ *
+ * main - is where out program starts from
+ *
+ * Return: (0) means that we end our program stops there 
+ *
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}

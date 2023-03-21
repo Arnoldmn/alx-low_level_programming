994a9ca63095f81d216c61fc7543b6b5bfc23a26
Arnoldmn <arnoldnzibuk@gmail.com>
2023-03-21 15:35:49 +0000

    Add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,11 +3,13 @@
 /**
  * add - Adds two numbers
  * @x: the first number to compute
+ *
  * @y: the second number to compute
+ *
  * Return: success result
  */
 
-int add(int x; int y)
+int add(int x, int y)
 {
 	return (x + y);
 }

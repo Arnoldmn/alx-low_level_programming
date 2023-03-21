3029f3c64a58fe1807ee843b0b978d91a022311f
Arnoldmn <arnoldnzibuk@gmail.com>
2023-03-21 11:18:02 +0000

    lower case error

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	int count = 0;
 	char alpha;
 
-	for (count = 0; count <= 10; count++)
+	for (count = 0; count <= 9; count++)
 	{
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
 		{

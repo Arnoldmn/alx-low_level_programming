93be01e29cdef55c976e5916fb33aaae2367c8f7
Arnoldmn <arnoldnzibuk@gmail.com>
2023-03-23 17:11:27 +0000

    print diago

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * print_lines - printing lines diagonal
+ * print_diagonal - printing lines diagonal
  * @n: input variable
  *
  * Return: void
  */
 
-void print_lines(int n)
+void print_diagonal(int n)
 {
 	int i, j;
 
-	if (n == 0)
+	if (n <= 0)
 		_putchar('\n');
 	for (i = 0; i < n; i++)
 	{
@@ -19,6 +19,7 @@ void print_lines(int n)
 			_putchar(' ');
 		}
 		_putchar('\\');
-		_putchar('\n')';
+		_putchar('\n');
 	}
+
 }

d6b924a8b2fc212f0843e4dfe88476c87bd7c9e6
arnoldmn <you@example.com>
2023-04-03 14:08:51 +0000

    main header

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _memcpy - copies n bytes from memory area src to memory area dest
+* @src: memory area src
+* @dest: memory area dest
+*
+* resturn - pointer to dest
+*/
+
+void *_memcpy(char *dest, char *src, unsignged int n)
+{
+
+}

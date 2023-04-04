17d328fc5bad2d67f6707f815bc832174b920fad
arnoldmn <you@example.com>
2023-04-04 02:16:36 +0000

    String to pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - sets string to pointer char
+ *
+ * @s: pointer to pointer
+ * @to: pointer char
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

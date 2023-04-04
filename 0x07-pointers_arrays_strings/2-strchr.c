7917cd61a10660ead8f7031f4d6c6b38ad2defe8
arnoldmn <you@example.com>
2023-04-04 05:54:05 +0000

    fix error

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,22 +1,24 @@
 #include "main.h"
-
 /**
  *_strchr - find a character in string
  *@c: character occurence
  *@s: pointer in string
  *Return: return pointer of the first character
  */
-
 void  *_strchr(char *s, char c)
 {
-	while (*s)
+	while (*s != '\0')
 	{
-		if (*s != c)
-			s++;
-		else
+		if (*s == c)
+		{
 			return (s);
+		}
+
+		s++;
 	}
-	if (c == '\0')
+	if (*S == c)
+	{
 		return (s);
-	return (NULL);
+	}
+	return (0);
 }

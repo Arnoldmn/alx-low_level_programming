3acd7debdf1c8552a03dfa94e1d17bb727fdc0b5
arnoldmn <you@example.com>
2023-04-05 23:53:42 +0000

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a recursive function
+ * @s: string
+ *
+ * Return: success(0)
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}

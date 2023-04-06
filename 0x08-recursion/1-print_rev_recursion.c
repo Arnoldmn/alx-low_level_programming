e53f2bc7797eb2ecd8616c553710c1bc7765faa3
arnoldmn <you@example.com>
2023-04-06 00:11:59 +0000

    reverse recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _prints_rev_recursion - string to printed in reverse
+ * @s: string pointer
+ * _putchar - prints out the character
+ *  Return: 0;
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}

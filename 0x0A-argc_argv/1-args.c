566c83e2782ba519a387b5c55c972fab1b5a5f79
arnoldmn <arnoldnzibuk@gmail.com>
2023-04-08 03:36:29 +0000

    arg count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ *
+ * @argv: vector argument
+ * @argc: argument count
+ *
+ * Return: Always (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}

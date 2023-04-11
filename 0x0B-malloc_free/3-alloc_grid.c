0d1cfd8b8e808770d05922ab85b80db1d4baa924
arnoldmn <arnoldnzibuk@gmail.com>
2023-04-11 19:51:55 +0000

    2D array error

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -28,11 +28,11 @@ int **alloc_grid(int width, int height)
 			free(p);
 			return (NULL);
 		}
-		for (j = 0; j < width; j++)
-		{
-			p[i][j] = 0;
-		}
 	}
+
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			p[i][j] = 0;
 	return (p);
 }
 

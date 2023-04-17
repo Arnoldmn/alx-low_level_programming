35537421d0a974f6fb5a26f168eb0b9cb83210f1
arnoldmn <arnoldnzibuk@gmail.com>
2023-04-17 16:06:35 +0000

    remove two data types

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,7 +7,7 @@
  * @owner:pointer who owns the dog
  */
 
-void void init_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{

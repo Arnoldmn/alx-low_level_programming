82928b5f11db70a8375a4204738825fe5f078325
arnoldmn <arnoldnzibuk@gmail.com>
2023-04-17 19:21:10 +0000

    fix 5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -31,7 +31,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog);
 		return (NULL);
 	}
-	for (x = 0; x < i; i++)
+	for (x = 0; x < i; x++)
 		dog->name[x] = name[x];
 	dog->age = age;
 	for (j = 0; owner[j]; j++)

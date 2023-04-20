fce20ca6004212d095e980966aff1e56f70a919a
arnoldmn <arnoldnzibuk@gmail.com>
2023-04-20 20:04:10 +0000

    print strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - print all strings
+ *
+ * @separator: character to separates strings
+ * @n: number of unsigned integers
+ * Return: Always 0
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	char *str;
+	unsigned int i;
+	va_list ij;
+
+	va_start(ij, n);
+	if (separator == NULL)
+		separator = "";
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ij, char*);
+		if (str == NULL)
+			str = "(nil)";
+		printf("%s", str);
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ij);
+}

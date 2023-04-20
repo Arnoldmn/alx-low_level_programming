40422c684cbdfea5e14cd97b861ca611c73bbfbb
arnoldmn <arnoldnzibuk@gmail.com>
2023-04-20 19:18:51 +0000

    prnt numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function that numbers
+ * @n: number of param
+ * 
+ * @separator: separator between two numbers
+ * Return: Always 0
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ij;
+
+	va_start(ij, n);
+
+	if (separator == NULL)
+		separator = "";
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ij, int));
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ij);
+}

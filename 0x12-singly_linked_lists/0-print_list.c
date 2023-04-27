1ddea7fedd7330021972c7c366d06c915e10c666
arnoldmn <arnoldnzibuk@gmail.com>
2023-04-27 19:07:20 +0000

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * list_len - returns number of elements
+ *
+ * @h: head of list
+ * Return: (count)
+ */
+
+size_t list_len(const list_t *h)
+{
+	const  list_t *current = h;
+	size_t count = 0;
+
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+			printf("[0] (nill)");
+		else
+			printf("[%d] %s", current->len, current->str);
+		count++;
+		printf("\n");
+		current = current->next;
+	}
+
+	return (count);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,23 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
+
+/**
+ * struct list_s - linked lis
+ * @str: string for storage of data
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node struct
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);

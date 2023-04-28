f3d13c5074e4fba6eb512196235feadf25372554
arnoldmn <arnoldnzibuk@gmail.com>
2023-04-28 02:09:38 +0000

    print message

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * print_message - prints message
+ * Return : void
+ */
+
+void print_message(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}

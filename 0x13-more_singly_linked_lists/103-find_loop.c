33485a3a3eb66159fabd2baf55f69aec9f6bbac4
arnoldmn <arnoldnzibuk@gmail.com>
2023-05-01 04:18:42 +0000

    err

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,12 +1,37 @@
 #include "lists.h"
+
 /**
- * find_listint_loop - print elements of a list in a save way
- * @head: pointer to list
- * Return: list
+ * find_listint_loop - Find loop in a linked list
+ * @head: head of the list
+ *
+ * Return: address of the node where the loop starts
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	if (head)
-		return (head);
-	exit(98);
+	listint_t *slow, *fast;
+
+	if (head == NULL || head->next == NULL)
+		return NULL;
+
+	slow = head->next;
+	fast = head->next->next;
+
+	while (fast && fast->next)
+	{
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return slow;
+		}
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+
+	return NULL;
 }
+
